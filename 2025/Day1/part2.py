d4d980004f99308583ea8eb9cee1b1e3d4ffe612
Michael Ivanov <michaelivanov@Michaels-MacBook-Air.local>
2025-12-01 14:49:31 -0500

    complete day 1

--- 2025/Day1/part2.py
+++ 2025/Day1/part2.py
@@ -11,9 +11,13 @@ for i in input:
     value = int(i[1:])
     if direction == 'L':
         value *= -1
+    sum = position + value
+    if (sum > 99):
+        solution += sum // 100
+    elif (sum <= 0 and position != 0):
+        solution += (abs(sum) // 100) + 1
+    elif (sum <= 0):
+        solution += (abs(sum) // 100)
     position = (position + value) % 100
-    if position == 0:
-        solution += 1
   
-
 print(solution)
